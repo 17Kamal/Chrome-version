68f8239f99af24c71c64f6f898d13ac0acac8684
kamal847409 <b170034@nitsikkim.ac.in>
2020-11-06 23:28:22 +0530

    improvement

--- app/src/main/java/com/example/chrome_version/MainActivity.java
+++ app/src/main/java/com/example/chrome_version/MainActivity.java
@@ -1,20 +1,10 @@
+/*author: Kamal Kumar */
 package com.example.chrome_version;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.regex.Pattern;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
